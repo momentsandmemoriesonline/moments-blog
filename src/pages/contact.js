ba8b9acd273f9a0721c0776e42b170039a6072f6
Robert Gallacher <rjgallac@hotmail.com>
2020-12-13 15:55:16 +0000

    update packages for CMS

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,7 @@ const ContactPage = ({ data, location }) => {
                 <div>
                   <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html}} />
                   <img 
-                  style={{border:"6px solid black", borderRadius: "20px", objectFit: "scale-down", width: "100%"}}
+                  style={{border:"6px solid #570430", borderRadius: "20px", objectFit: "scale-down", width: "100%"}}
                   src={data.allMarkdownRemark.edges[0].node.frontmatter.map}></img>
                 </div>
             </main>
