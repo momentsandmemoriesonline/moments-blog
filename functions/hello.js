648a64db3140bf9c4d299c807c2a13d173590d43
Robert Gallacher <rjgallac@hotmail.com>
2020-12-14 21:22:27 +0000

    try again with json stringify

--- functions/hello.js
+++ functions/hello.js
@@ -2,17 +2,14 @@ const googleAuth = require('./google-auth');
 const {getBookable, getBooked} = require("./google-calendar-function");
 const {splitSlots, removeConflicts} = require("./slot-util");
 
-exports.handler = (event, context) => {
+exports.handler = async(event, context) => {
     const { name = "Anonymous" } = event.queryStringParameters;
-    console.log("HERE2")
     googleAuth.login()
     .then(auth => Promise.all([getBookable(auth), getBooked(auth)])
     .then(data => {
-      console.log("HERE2")
-      console.log(removeConflicts(splitSlots(data[0]), data[1]));
       return {
         statusCode: 200,
-        body: removeConflicts(splitSlots(data[0]), data[1])
+        body: JSON.stringify(removeConflicts(splitSlots(data[0]), data[1]))
       };
     }));  
 
