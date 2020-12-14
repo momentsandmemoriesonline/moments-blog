bb8db2c8d3ebe893a1d6de41e5e5348ddfd29ea0
Robert Gallacher <rjgallac@hotmail.com>
2020-12-14 21:39:25 +0000

    does this work

--- functions/hello.js
+++ functions/hello.js
@@ -2,9 +2,9 @@ const googleAuth = require('./google-auth');
 const {getBookable, getBooked} = require("./google-calendar-function");
 const {splitSlots, removeConflicts} = require("./slot-util");
 
-exports.handler = (event, context) => {
+exports.handler = async(event, context) => {
     const { name = "Anonymous" } = event.queryStringParameters;
-    googleAuth.login()
+    return googleAuth.login()
     .then(auth => Promise.all([getBookable(auth), getBooked(auth)])
     .then(data => {
       console.log(JSON.stringify(removeConflicts(splitSlots(data[0]), data[1])))
