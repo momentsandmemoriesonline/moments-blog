2270b0cfb1e35f2953f45eb42e2411c60eaf9062
Robert Gallacher <rjgallac@hotmail.com>
2020-12-14 21:06:18 +0000

    console logs for debugging

--- functions/hello.js
+++ functions/hello.js
@@ -4,11 +4,12 @@ const {splitSlots, removeConflicts} = require("./slot-util");
 
 exports.handler = async (event, context) => {
     const { name = "Anonymous" } = event.queryStringParameters;
-
+    console.log("HERE2")
     googleAuth.login()
     .then(auth => Promise.all([getBookable(auth), getBooked(auth)])
     .then(data => {
-      
+      console.log("HERE2")
+      console.log(removeConflicts(splitSlots(data[0]), data[1]));
       return {
         statusCode: 200,
         body: removeConflicts(splitSlots(data[0]), data[1])
