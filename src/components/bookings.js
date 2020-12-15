6e9720713eca60c88eb49b8397bf59f19b776a4a
Robert Gallacher <rjgallac@hotmail.com>
2020-12-15 22:29:24 +0000

    add telephone and hide panels

--- src/components/bookings.js
+++ src/components/bookings.js
@@ -6,11 +6,16 @@ class Bookings extends React.Component{
         this.state = {
             bookings:[],
             currentBooking:{'summary':'Family'},
-            name:""
+            name:"",
+            tel: "",
+            showDetail: false,
+            showSlots: true,
+            showBooked: false
         }
         this.book = this.book.bind(this);
         this.bookDetails = this.bookDetails.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleTelChange = this.handleTelChange.bind(this);
     }
 
     componentDidMount() {
@@ -26,7 +31,13 @@ class Bookings extends React.Component{
         this.setState({name: event.target.value});
     }
 
+    handleTelChange(event) {
+        this.setState({tel: event.target.value});
+    }
+
     book(booking) {
+        this.setState({showSlots: false})
+        this.setState({showDetail: true})
         const newbooking = {
             'start': {
                 'dateTime': new Date(booking.start).toISOString()
@@ -40,15 +51,15 @@ class Bookings extends React.Component{
 
     bookDetails(){
         let copyBooking = { ...this.state.currentBooking};
-        copyBooking.description = this.state.name;
+        copyBooking.description = this.state.name + this.state.tel;
         copyBooking.summary = 'Family';
         fetch('/api/book', {
             method: 'post',
             body: JSON.stringify(copyBooking)
         })
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({bookings: data})
+        .then(res => {
+            this.setState({showDetail: false})
+            this.setState({showBooked: true})
         })
         .catch(console.log)
     }
@@ -56,14 +67,19 @@ class Bookings extends React.Component{
     render(){
         return (
             <div>
-                <div className="bookingDetail">
+                <div >
+                    <h1 style={this.state.showBooked ? {} : { display: 'none'} }>BOOKED</h1>
+                </div>
+                <div className="bookingDetail" style={this.state.showDetail ? {} : { display: 'none'} }>
                     {this.state.currentBooking?.start?.dateTime}
                     {this.state.currentBooking?.end?.dateTime}
                     <br />
                     Name: <input value={this.state.name} onChange={this.handleChange} />
+                    <br />
+                    Telephone: <input value={this.state.tel} onChange={this.handleTelChange} />
                     <button className="booking-btn" onClick={() => this.bookDetails()}>Book</button>
                 </div>
-                <div className="slots">
+                <div className="slots" style={this.state.showSlots ? {} : { display: 'none'} }>
                     {this.state.bookings.map((booking, index) => {
                         return(
                             <p className="slot" key={index}> {new Date(booking.start).toLocaleDateString()} : {new Date(booking.start).toLocaleTimeString()} - {new Date(booking.end).toLocaleTimeString()}
