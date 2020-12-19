4a4ed62052063e5d2f4132d886def316efad57c2
Robert Gallacher <rjgallac@hotmail.com>
2020-12-19 22:22:36 +0000

    still trying

--- functions/book.js
+++ functions/book.js
@@ -1,20 +1,13 @@
 const googleAuth = require('./google-auth');
 const {book} = require("./google-calendar-function");
 
-exports.handler = async(event, context, callback) => {
-  console.log(event.body)
-    const { name = "Anonymous" } = event.queryStringParameters;
+exports.handler = async(event) => {
     var photoEvent = JSON.parse(event.body);
-    console.log(photoEvent)
-    googleAuth.login()
+    await googleAuth.login()
     .then(auth => {
       book(auth, photoEvent)
       .then(data => {
         console.log("in here" + data)
-        callback(null, {
-          statusCode: 200,
-          body: "BOOKED"
-        });
       })
       .catch(err => {
         console.log(err)
@@ -22,4 +15,8 @@ exports.handler = async(event, context, callback) => {
       });
       
     });  
+    return {
+      statusCode: 200,
+      body: "BOOKED"
+    };
   };
