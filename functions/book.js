7f36bde13d43f8a8f5a288b0d50e6439deacd9d2
Robert Gallacher <rjgallac@hotmail.com>
2020-12-19 22:13:24 +0000

    does netlify lamda not support promises

--- functions/book.js
+++ functions/book.js
@@ -1,20 +1,20 @@
 const googleAuth = require('./google-auth');
 const {book} = require("./google-calendar-function");
 
-exports.handler = async(event, context) => {
+exports.handler = async(event, context, callback) => {
   console.log(event.body)
     const { name = "Anonymous" } = event.queryStringParameters;
     var photoEvent = JSON.parse(event.body);
     console.log(photoEvent)
-    return googleAuth.login()
+    googleAuth.login()
     .then(auth => {
       book(auth, photoEvent)
       .then(data => {
         console.log("in here" + data)
-        return {
+        callback(null, {
           statusCode: 200,
           body: "BOOKED"
-        };
+        });
       })
       .catch(err => {
         console.log(err)
